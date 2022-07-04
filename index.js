e874642eec9c9bb3a33a939e6bcec14baa8dcbc5
EliyahuCohen <100947769+EliyahuCohen@users.noreply.github.com>
2022-07-04 15:06:18 +0300

    this is better1

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const importData = require("./data.json");
 
 const port = process.env.PORT || 3000;
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.get("/", (req, res) => {
   res.send("Hey this is NBA players API 2022 season");
 });
 app.get("/players", (req, res) => {
-  res.send(importData);
+  res.send(importData).json(importData);
 });
 app.listen(port, (req, res) => {
   console.log("hey app is listening on port http://localhost:" + port);
